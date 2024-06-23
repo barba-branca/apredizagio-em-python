ca117ec88ea217af4c5dfcaed248744fb21c4c45
KAUE NEVES <137438983+barba-branca@users.noreply.github.com>
2024-06-22 22:55:22 -0300

    ativudades

---
+++ aula6.py
@@ -0,0 +1,44 @@
+# na programaçao existe uma coisa que tem varios nomes
+
+# conversa de tipos, coerção
+# type convertion,typecasting, coercion 
+# seria basicamente converter um tipo em outro tipo
+# quando voce soma 
+print(1+1)
+# o python reconhece isso como um inteiro e faz a soma 
+# quando voce soma
+print('a' + 'b')
+# python reconhece isso como uma str e concatena
+# concatenar significa juntar 2 coisas
+# isso se chama polimorfismo ele ta utilisando 1 operador de soma para 2 situaçaopra fazer coisas diferentes
+# isso so acontece em linguagens dinamicas
+# pq vc passa dois tipos para o interpretador e ele sabe o que fazer com esses 2 tipos
+print(int('1') + 1)
+# o python nao pode fazer pq ele nao concatena uma str com um int
+
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+# colando dentro de uma funçao print
+print('1', type(1))
+# e colocar um valor e o tipo do daquele valor o python vai entender que é uma string 
+# se a gente quizer fazer que ele vire um inteiro e so colocar um int
+print(int('1'), type(int('1')))
+# precisa colocar o int nos duas classes para ele saber que fez a coeçao
+# se fizermos:
+print(int('1') + 1)
+# assim ele entende que é um int
+# o python vai indentificar essa string como um inteiro e fazer a soma dos dois
+# o python tambem pode entende como float se a gente mudar o int pra float
+print(type(float('1') + 1))
+# assim ele entende que é um float
+# sempre que a gente ver um numero com um ponto, podemos indentificar que estamos falando de um float
+# quando a gente colocar parentese dentro de parentese o python primeiro vai ler o parentese do meio depos os outros
+# tipo assim (terceiro(segundo(priomeiro)))
+print(bool(''))
+# so tem dois valores True ou False 
+# ou seja ele vai indentificar se aquilo é verdadeiro ou falso
+# uma funçao com o bolean vazia é False ja uma funçao com a bolean que tem um valor (pode ser um espaço) é True
+print(11 + 'b')
+# o python nao vai entender que voce esta passando uma str pra ele, ou seja, ele nao vai concatenar esse valor
+print(str(11) + 'b')
+# pra ele indentificar indentificar esse dois valores e string a gente precisa colocar o str para ele concatenar a string
