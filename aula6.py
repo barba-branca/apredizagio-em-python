17579a7dc09d4c295491084f82ef5d8c156f13c5
KAUE NEVES <137438983+barba-branca@users.noreply.github.com>
2024-06-22 23:25:52 -0300

    Update aula6.py

--- aula6.py
+++ aula6.py
@@ -10,17 +10,17 @@ print(1+1)
 print('a' + 'b')
 # python reconhece isso como uma str e concatena
 # concatenar significa juntar 2 coisas
-# isso se chama polimorfismo ele ta utilisando 1 operador de soma para 2 situaçaopra fazer coisas diferentes
+# isso se chama polimorfismo ele ta utilisando 1 operador de soma para 2 situaçao pra fazer coisas diferentes
 # isso so acontece em linguagens dinamicas
 # pq vc passa dois tipos para o interpretador e ele sabe o que fazer com esses 2 tipos
-print(int('1') + 1)
+print(('1') + 1)
 # o python nao pode fazer pq ele nao concatena uma str com um int
 
 # tipos imutáveis e primitivos:
 # str, int, float, bool
-# colando dentro de uma funçao print
+# colocando dentro de uma funçao print
 print('1', type(1))
-# e colocar um valor e o tipo do daquele valor o python vai entender que é uma string 
+# e colocar um valor e o tipo daquele valor o python vai entender que é uma string 
 # se a gente quizer fazer que ele vire um inteiro e so colocar um int
 print(int('1'), type(int('1')))
 # precisa colocar o int nos duas classes para ele saber que fez a coeçao
@@ -32,7 +32,7 @@ print(int('1') + 1)
 print(type(float('1') + 1))
 # assim ele entende que é um float
 # sempre que a gente ver um numero com um ponto, podemos indentificar que estamos falando de um float
-# quando a gente colocar parentese dentro de parentese o python primeiro vai ler o parentese do meio depos os outros
+# quando a gente colocar parentese dentro de parentese o python primeiro vai ler o parentese do meio depois os outros
 # tipo assim (terceiro(segundo(priomeiro)))
 print(bool(''))
 # so tem dois valores True ou False 
@@ -41,4 +41,4 @@ print(bool(''))
 print(11 + 'b')
 # o python nao vai entender que voce esta passando uma str pra ele, ou seja, ele nao vai concatenar esse valor
 print(str(11) + 'b')
-# pra ele indentificar indentificar esse dois valores e string a gente precisa colocar o str para ele concatenar a string
+# pra ele indentificar indentificar esse dois valores em string a gente precisa colocar o str para ele concatenar a string
